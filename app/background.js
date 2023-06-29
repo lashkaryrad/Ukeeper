8ecf24512e168447a749e035126f15f97ea11e84
lashkaryrad <lashkaryrad@gmail.com>
2023-06-29 19:26:36 +0330

    Deleting background & buttons.js

--- app/background.js
+++
@@ -1,8 +0,0 @@
-let showCurrentURL = document.getElementById("action-url");
-var currentSiteURL;
-
-chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-  var currentSiteURL = tabs[0].url;
-  showCurrentURL.value = currentSiteURL;
-});
-
