469fe2fa227f6a4c9eda61f47e6b1ce1f747e4b7
lashkaryrad <lashkaryrad@gmail.com>
2023-06-29 19:19:12 +0330

    Merging Buttons.js and source.js togather

--- app/source.js
+++ app/source.js
@@ -1,5 +1,6 @@
 /*   ----   Core   ----    */
 let actionURL = document.getElementById("action-url"); // Define actionURL input.
+var addButton = document.getElementById('add-url'); //Define Add Buttons
 var currentSiteURL; // Define current Site's URL.
 
 // Getting current Site's URL from chrome Tabs.
@@ -8,5 +9,44 @@ chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
   actionURL.value = currentSiteURL;
 });
 
+/*   ----   Save Current URL    ----    */
+document.addEventListener('DOMContentLoaded', function() {
+  // Define Variables
+  var addButton = document.getElementById('add-url');
+  var actionURL = document.getElementById('action-url')
+  var addIcon = document.querySelector('.octicon-add');
+  var checkIcon = document.querySelector('.octicon-check');
+
+  addButton.addEventListener('click', function() {
+    // Do adding URL, here...
+
+    // addIcon animation 
+    addIcon.style.display = 'none';
+    checkIcon.style.display = 'inline';
+
+    setTimeout(function() {
+      addIcon.style.display = 'inline';
+      checkIcon.style.display = 'none';
+    }, 1000);
+  });
+});
+
+/*   ----   Styles for Doing add URL.   ----    */
+actionURL.addEventListener("mouseover", () => {
+  addButton.style.display = 'block';
+});
+
+actionURL.addEventListener("mouseout", () => {
+  addButton.style.display = 'none';
+});
+
+
+addButton.addEventListener("mouseover", () => {
+  addButton.style.display = 'block';
+});
+
+addButton.addEventListener("mouseout", () => {
+  addButton.style.display = 'none';
+});
 
 
